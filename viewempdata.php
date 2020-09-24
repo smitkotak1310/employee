bfe3cfbafa5de993834693dad934c6547b2b779d
smitkotak1310 <43035695+smitkotak1310@users.noreply.github.com>
2020-09-24 14:24:15 +0530

    Update viewempdata.php

--- viewempdata.php
+++ viewempdata.php
@@ -1,17 +1,18 @@
 <?php 
 include 'server.php';
-echo "<h3> Employee Details</h3>";
+echo "<h3>Details</h3>";
   $show_query="select * from emp_info";
   $result=mysqli_query($db,$show_query);
  
-echo "<table border=2><tr bgcolor='#000'><td>Emp First Name</td><td>Emp Last Name</td><td>Email</td><td>Mobile Number</td></tr>";
+echo "<table border=2><tr bgcolor='#e2f24f'><td>First Name</td><td>Last Name</td><td>Email</td><td>Mobile Number</td></tr>";
 
  while ($row=mysqli_fetch_array($result)) {
-
+  
+      echo '<tr>
+        <td> '.$row[0].' </td>
         <td> '.$row[1].' </td>
         <td> '.$row[2].' </td>
         <td> '.$row[3].' </td>
-        <td> '.$row[4].' </td>
          </tr> ';
     }
     echo "</table>";
